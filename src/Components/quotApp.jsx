eb0d8df014351b8b0359787a9f97af67acb202b2
Hamed Namazi <H.namazi91@gmail.com>
2025-02-15 16:49:20 +0330

    Basic display template - colorless

--- src/Components/quotApp.jsx
+++ src/Components/quotApp.jsx
@@ -4,7 +4,24 @@ class QuotApp extends Component {
     state = {  } 
     render() { 
         return (
-            <h1>Helo From Me</h1>
+<>
+<div className="container p-4">
+                <h2><i className="fa-solid fa-quote-left pe-3"></i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, vel cumque iure rerum, dolorum sed mollitia minima autem natus omnis officia? Optio pariatur ea vitae repellendus sit sequi voluptatibus aut?</h2>
+                <p className="text-end py">Author name</p>
+                <div className="row justify-content-between">
+                    <div className='col'>
+                    <button className='btn rounded-2'><i className="fa-brands fa-twitter"></i></button>
+                    <button className='btn rounded-2'><i className="fa-brands fa-tumblr"></i></button>
+
+                    </div>
+                    <div className="col text-end">
+                        <button className="btn rounded-2">New Quote</button>
+                    </div>
+
+                </div>
+            </div>
+            <div className="text-center"><p>by Hamed</p></div>
+            </>
         );
     }
 }
